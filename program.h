d8aee37330b49c0d0dd4c020d2074e0601521a56
Palliser7 <56008018+Palliser7@users.noreply.github.com>
2019-10-09 19:05:07 -0600

    Update program.h
    pretty much copied what was in the usefulcodemaybe.cpp

--- program.h
+++ program.h
@@ -1,29 +1,11 @@
 #ifndef program_h
 #define program_h
 
-#include <iostream>
-#include <fstream>
 #include <string>
-#include <stdlib.h>
 
 using namespace std;
-
-const int MAX_SIZE = 50;
-
-struct node
-{
-	node* next;
-	char dta;
+struct instruction {
+	string operation;
+	char *operand;
 };
-
-struct instructions {
-	int pc;
-	string op = "";
-	node* symbol;
-};
-
-string getFile();
-
-void readFile(string, instructions);
-
 #endif
